0b6e2259ee52bc702038caba098f686391734c27
Amisha Pai <amishapaib@gmail.com>
2024-01-08 15:45:58 +0530

    Skill-Lab-CP

---
+++ Contest.py
@@ -0,0 +1,69 @@
+'''num=int(input())
+for i in range (0,num):
+    dig=[]
+    for i in range (0,3):
+        dig.append(input())
+    c=[]
+    dig=list(dig)
+    for i in dig:
+        if i in c:
+            dig.remove(i)
+            for j in dig:
+                if j==i:
+                    dig.remove(i)
+        else:
+            c.append(i)
+    print(dig[0])'''
+
+
+'''num=int(input())
+for i in range (0,num):
+    dig=[]
+    for i in range (0,3):
+        dig.append(input())
+    c=[]
+    dig=list(dig)
+    if dig[0]==dig[1]:
+        print(dig[2])
+    elif dig[1]==dig[2]:
+        print(dig[0])
+    else:
+        print(dig[1])'''
+
+
+'''num=int(input())
+for i in range (0,num):
+    dig=input()
+    dig= ''.join(list(map(lambda x: x.strip(), dig.split())))
+    c=[]
+    dig=list(dig)
+    if dig[0]==dig[1]:
+        print(dig[2])
+    elif dig[1]==dig[2]:
+        print(dig[0])
+    else:
+        print(dig[1])'''
+
+
+
+
+l=['a','b','c','d','e','f','g','h']
+r=[1,2,3,4,5,6,7,8]
+num=int(input())
+for j in range (0,num):
+    pos=input()
+    for i in r:
+        if int(i)==int(pos[1]):
+            continue;
+        else:
+            print(str(pos[0]+str(i)))
+    for i in l:
+        if str(i)==str(pos[0]):
+            continue;
+        else:
+            print(str(i)+str(pos[1]))
+
+ 
+
+
+
---
+++ Robber.py
@@ -0,0 +1,5 @@
+#You are a professional robber planning to rob houses along a street. each house has a certain amount of money stashed, the only constraint stopping you
+#from robbing each of them is that adjacent houses. Given an integer array nums reprsenting smount of money in each house, return the max amount you can rob.
+
+
+#You have infinite amount of rupees 1 and 2 coins. Find the number of ways to make amount n. Eg: 3(1+1+1, 1+2, 2+1), gives fibonnaci
